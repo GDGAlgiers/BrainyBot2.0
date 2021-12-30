7b2f4d9d89247b22fc23dd9ac20f44fe4fb9f8f0
abdelghani <a.meliani@esi-sba.dz>
2021-12-30 06:32:22 +0100

    off res done

---
+++ cogs/techpoint/offres.js
@@ -0,0 +1,60 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const { MessageEmbed } = require('discord.js');
+const path = require('path');
+const fs = require("fs");
+var date = new Date();
+
+function session_active() {
+    console.log(__dirname)
+    return fs.existsSync(__dirname + "/tmp" + "/" + date.toDateString() + ".md")
+}
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('offres')
+        .setDescription('add a resource')
+        .addStringOption(option =>
+            option.setName('link')
+            .setDescription("the resource's link")
+            .setRequired(true))
+        .addStringOption(option =>
+            option.setName('descreption')
+            .setDescription("descreption")
+            .setRequired(true)),
+    async execute(interaction) {
+        const link = interaction.options.getString('link');
+        const descreption = interaction.options.getString('descreption');
+
+        if (!session_active()) {
+
+            const errorembed = new MessageEmbed()
+                .setColor('#0099ff')
+                .setTitle('ERROR ❌')
+                .setDescription('a techpoint session must be active to take a note');
+
+            const channel = interaction.client.channels.cache.get('925210219338928169');
+
+
+            channel.send({ embeds: [errorembed] })
+
+        } else {
+
+            fs.appendFileSync(__dirname + "/tmp" + "/off_resources" + ".md",
+                '\n---\n' + link + ' ' + descreption + ' added by ' + interaction.user.username + '\n---\n',
+                "UTF-8", { 'flags': 'a+' });
+
+
+            const errorembed = new MessageEmbed()
+                .setColor('#0099ff')
+                .setTitle('RESOURCE ADDED')
+                .setDescription(descreption)
+                .setURL(link);
+
+            const channel = interaction.client.channels.cache.get('925210219338928169');
+
+
+            channel.send({ embeds: [errorembed] })
+        }
+    },
+
+};
--- cogs/techpoint/tmp/off_resources.md
+++ cogs/techpoint/tmp/off_resources.md
@@ -1,1 +1,12 @@
 OFF RESOURCES
+---
+www.github.com/abdelghanimeliani hahaha added by abdelghanimeliani
+---
+
+---
+www.github.com aaaaaa added by abdelghanimeliani
+---
+
+---
+https://github.com/abdelghanimeliani?tab=followers my github account added by abdelghanimeliani
+---
