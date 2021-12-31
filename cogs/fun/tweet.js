07f1761257bf3333b7032abcde12aeebb681d579
AlmammaAmir <56568152+amir-coder@users.noreply.github.com>
2021-12-31 14:51:46 +0100

    Implemented: tweet adnd advice fun commands

--- cogs/fun/tweet.js
+++ cogs/fun/tweet.js
@@ -1,39 +1,55 @@
 const { SlashCommandBuilder } =  require('@discordjs/builders');
 
 //utils
-const { https } = require('https');
+const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
+const { MessageEmbed } = require('discord.js');
+const {get_advice} = require("../../core/utils");
 
 module.exports = {
     data: new SlashCommandBuilder()
     .setName('tweet')
     .setDescription('You can tweet as someone else to troll others')
-    .addStringOption(option => option.setName('account').setDescription('Enter the tweeter user"s account') )
-    .addStringOption(option => option.setName("text").setDescription("The content of your tweet") ),
+    .addStringOption(option => option.setName('account').setDescription('Enter the tweeter user"s account').setRequired(true) )
+    .addStringOption(option => option.setName("text").setDescription("The content of your tweet").setRequired(true) ),
     async execute(interaction) {
 
-        const url = `https://nekobot.xyz/api/imagegen?type=tweet&username=${interaction.options.getString('account')}&text=${interaction.options.getString('text')}`;
+        const account = interaction.options.getString('account');
 
-        const request = new https.request(url, (response) => {
-            let data = '';
+        const text = interaction.options.getString('text');
 
-            response.on('data', (chunck) => {
-                data = data + chunck.toString();
-            });
+        if(!account) {
+            await interaction.reply("Your Tweet must have an Account name");
+            await interaction.followUp(`Example: /tweet account:Brainy text:${get_advice()}`);
 
-            response.on('end', () => {
-                const body = JSON.parse(data);
-                console.log(` log: ${body}`);
-            });
-        });
+            //throwing exeption of command syntax error
+            return;
+        }
 
-        request.on('error', (error) => {
-            console.log('An Error', error);
-        });
+        if(!text) {
+            await interaction.reply("Your Tweet must have an text content");
+            await interaction.followUp(`Example: /tweet account:Brainy text:${get_advice()}`);
 
-        request.end() ;
+            //throwing exeption of command syntax error
+            return;
+        }
 
-        console.log(`User: ${interaction.options.getString('account')} Text: ${interaction.options.getString('text')} `);
-        await interaction.reply(`User: ${body} Text: ${interaction.options.getString('text')} `);
+        //getting the image URL:
+        const url = `https://nekobot.xyz/api/imagegen?type=tweet&username=${account}&text=${text}`;
+        const res = await fetch(url);
+        const data = await res.json();
+        const imageUrl = data["message"];
+        
+        //Embeding the image in An Embeded message
+        const EmbededTweet = new MessageEmbed()
+        .setColor('0x00FF00')
+        .setTitle(`You made ${account} tweet this:`)
+        .setImage(imageUrl)
+        .setTimestamp();
+
+        //replying with the Embeded message
+        await interaction.reply({embeds: [EmbededTweet]});
     }
 };
 
+
+  
