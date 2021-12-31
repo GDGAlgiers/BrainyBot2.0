97b2c9790cc6203acab0c7419f8da2118524743e
AlmammaAmir <56568152+amir-coder@users.noreply.github.com>
2021-12-31 12:49:45 +0100

    implementing tweet command

--- cogs/fun/tweet.js
+++ cogs/fun/tweet.js
@@ -1,9 +1,39 @@
 const { SlashCommandBuilder } =  require('@discordjs/builders');
 
+//utils
+const { https } = require('https');
 
 module.exports = {
-    data: new SlashCommandBuilder().setName('tweet').setDescription('You can tweet as someone else to troll others'),
+    data: new SlashCommandBuilder()
+    .setName('tweet')
+    .setDescription('You can tweet as someone else to troll others')
+    .addStringOption(option => option.setName('account').setDescription('Enter the tweeter user"s account') )
+    .addStringOption(option => option.setName("text").setDescription("The content of your tweet") ),
     async execute(interaction) {
-        await interaction.reply("");
+
+        const url = `https://nekobot.xyz/api/imagegen?type=tweet&username=${interaction.options.getString('account')}&text=${interaction.options.getString('text')}`;
+
+        const request = new https.request(url, (response) => {
+            let data = '';
+
+            response.on('data', (chunck) => {
+                data = data + chunck.toString();
+            });
+
+            response.on('end', () => {
+                const body = JSON.parse(data);
+                console.log(` log: ${body}`);
+            });
+        });
+
+        request.on('error', (error) => {
+            console.log('An Error', error);
+        });
+
+        request.end() ;
+
+        console.log(`User: ${interaction.options.getString('account')} Text: ${interaction.options.getString('text')} `);
+        await interaction.reply(`User: ${body} Text: ${interaction.options.getString('text')} `);
     }
 };
+
