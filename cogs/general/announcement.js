bb186c15ac2893fb1fe144f7e1e45a5c577d0450
imanehmz <78013834+imanehmz@users.noreply.github.com>
2022-01-06 15:18:01 +0100

    v 1.1

--- cogs/general/announcement.js
+++ cogs/general/announcement.js
@@ -1,4 +1,6 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
+const { channel } = require('diagnostics_channel');
+const { Client, ClientUser } = require('discord.js');
 
 const wait = require('util').promisify(setTimeout);
 module.exports = {
@@ -9,46 +11,40 @@ module.exports = {
         option.setName('channel_name')
         .setDescription('Name of the channel where to announce')
         .setRequired(true)),
-    async execute(interaction) {
+    async execute(interaction,client) {
         try {
+            await interaction.deferReply("wait for the bot");
+            const channel_name = interaction.options.getString('channel_name'); 
+            //trying to cast thechannel name from string to channel
+            const announcement_channel = client.channels.cache.get(channel => channel.name === channel_name);
+            console.log(announcement_channel.name);
+            //const announcement_channel = client.cache.find(channel => channel.name === channel_name)
             //The command should be executed at the #bot_announcements channel
             if(interaction.channel.name !== "général"){
                 await interaction.reply("You're at the wrong channel!");
             }
             console.log(interaction.channel.name);
             interaction.channel.send("Send the message and the image here");
-            await wait(15000);
-            const filter = m =>true;
+            const filter = m => !m.author.bot; //ignore bot messages
             const collector = interaction.channel.createMessageCollector({ filter, time: 15000 });
             let counter = 0;
             collector.on('collect', (m) => {
                 console.log(`Collected ${m.content}`);
+                if(counter === 0){
+                    announcement_channel.send(m.content);
+                }
                 interaction.channel.send("sent element");
                 counter ++;
                 if(counter>2){
                     collector.stop();
                 }
             });
-
+            
             /*collector.on('end',(m) => {
                 console.log(`Collected ${m.size} items`);
             });*/
-            if(m.size === 1){
-                //sending the message only
-                announcement_channel.send(m.last());
-            }
-            if(collected.size === 2){
-                //sending message and image
-                image = m.last;
-                message = m.last;
-                //won't work in here but i'll fix it
-                announcement_channel.send(image);
-                announcement_channel.send(message);
-            }
             await wait(15000);
             //interaction.followUp(`${m.first().author} got the correct answer!`);
-            const channel_name = interaction.options.getString('channel_name'); 
-            announcement_channel = `${channel_name}`;
             await interaction.editReply(`Your message has been successfully announced at ${channel_name}`);
         } catch (error) {
             console.log(error);
