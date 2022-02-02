6f96a2409ca3c03f0553f0529a46ae0731bad414
m0kr4n3 <m0kr4n3@m0kr4n3.m0kr4n3>
2022-02-02 18:08:37 -0500

    adding comments

--- src/commands/general/IsSpotOpen.js
+++ src/commands/general/IsSpotOpen.js
@@ -1,35 +1,38 @@
-const {MessageEmbed} = require('discord.js');
-const {getSpot} = require('../../core/spot');
+const { MessageEmbed } = require('discord.js');
+const { getSpot } = require('../../core/spot');
 
 module.exports = {
-  name: 'is_spot_open',
-  description: 'check if the GDG Algiers spot is open or not',
-  execute: async (client, interaction) => {
-    const replyEmbed = new MessageEmbed().setAuthor(
-        'GDG Algiers Spot',
-        'https://www.gdgalgiers.com/static/phonelogo-db9c725b1463afd46d9b886076124bb2.png',
-        'https://goo.gl/maps/Xgcq2nossHZG4Guy9',
-    );
+    name: 'is_spot_open',
+    description: 'check if the GDG Algiers spot is open or not',
+    execute: async(client, interaction) => {
+        const replyEmbed = new MessageEmbed().setAuthor(
+            'GDG Algiers Spot',
+            'https://www.gdgalgiers.com/static/phonelogo-db9c725b1463afd46d9b886076124bb2.png',
+            'https://goo.gl/maps/Xgcq2nossHZG4Guy9',
+        );
 
-    const spot = getSpot();
-    if (spot) {
-      await interaction.reply({
-        embeds: [
-          replyEmbed
-              .setDescription('The spot is currently **open**!')
-              .setColor('GREEN'),
-        ],
-        ephemeral: true,
-      });
-    } else {
-      await interaction.reply({
-        embeds: [
-          replyEmbed
-              .setDescription('The spot is currently **closed**!')
-              .setColor('RED'),
-        ],
-        ephemeral: true,
-      });
-    }
-  },
+        // get the value of SPOT 
+        const spot = getSpot();
+
+        //reply to the user with spot state
+        if (spot) {
+            await interaction.reply({
+                embeds: [
+                    replyEmbed
+                    .setDescription('The spot is currently **open**!')
+                    .setColor('GREEN'),
+                ],
+                ephemeral: true,
+            });
+        } else {
+            await interaction.reply({
+                embeds: [
+                    replyEmbed
+                    .setDescription('The spot is currently **closed**!')
+                    .setColor('RED'),
+                ],
+                ephemeral: true,
+            });
+        }
+    },
 };
--- src/core/spot/index.js
+++ src/core/spot/index.js
@@ -2,23 +2,24 @@ const fs = require('fs');
 const path = require('path');
 /**
  *
- * @return {*}
+ * @return {boolean} - the value of SPOT variable in the config.json file
  */
 const getSpot = () => {
-  return JSON.parse(fs.readFileSync(path.resolve('config.json')))['SPOT'];
+    return JSON.parse(fs.readFileSync(path.resolve('config.json')))['SPOT'];
 };
 
 /**
- *
- * @param {*} value
+ * @param {boolean} value - a boolean value to be assigned to SPOT variable in the config.json file
+ * @return {*} 
  */
 const setSpot = (value) => {
-  filePath = path.resolve('config.json');
-  config = JSON.parse(fs.readFileSync(file_path));
-  config['SPOT'] = value;
-  fs.writeFileSync(file_path, JSON.stringify(config, null, '\t'));
+    let file_path = path.resolve('config.json');
+    let config = JSON.parse(fs.readFileSync(file_path));
+    config['SPOT'] = value;
+    fs.writeFileSync(file_path, JSON.stringify(config, null, '\t'));
+    return 0;
 };
 module.exports = {
-  getSpot,
-  setSpot,
+    getSpot,
+    setSpot,
 };
