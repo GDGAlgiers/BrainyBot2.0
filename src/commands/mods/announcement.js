bc57d852cf1e8aa36fe039a3f11cfdcbada09cec
ahmedmokeddem <63117505+ahmedmokeddem@users.noreply.github.com>
2022-02-04 16:41:36 +0100

    Fix: Footer added to the annoucement embed(s)

--- src/commands/mods/announcement.js
+++ src/commands/mods/announcement.js
@@ -2,8 +2,6 @@ const { MessageEmbed } = require('discord.js');
 const { ephemeral } = require('../../utils');
 
 
-const wait = require('util').promisify(setTimeout);
-
 module.exports = {
     name: 'announce',
     description: 'Announce a message in a specific channel',
@@ -30,16 +28,15 @@ module.exports = {
                 return;
             }
 
-            await interaction.editReply(ephemeral('Send the text and the image (in one message) in this channel now!'));
-
+            const interact = await interaction.editReply({ content: 'Send the text and the image (in one message) in this channel now!', fetchReply: true, ephemeral: true });
             const filter = (m) => m.author.id === interaction.member.id;
             const collector = interaction.channel
                 .createMessageCollector({ filter, max: 1, time: 90 * 1000 });
             collector.on('collect', async(m) => {
                 if (m) {
-                    //generate random color for the embed (s)
-                    color = "#" + ((1 << 24) * Math.random() | 0).toString(16);
-                    //create the embeds, each embed includes one attachement
+                    // generate random color for the embed (s)
+                    color = '#' + ((1 << 24) * Math.random() | 0).toString(16);
+                    // create the embeds, each embed includes one attachement
                     if (m.attachments.size > 0) {
                         embeds = [];
                         await m.attachments.forEach((msgAttach) => {
@@ -48,20 +45,26 @@ module.exports = {
                                 .setImage(msgAttach.url);
                             embeds.push(embeddedMsg);
                         });
-                        //send the embeds
-                        channelName.send({
-                            embeds: [embeds[0].setDescription(m.content).setAuthor('GDG Algiers', 'https://www.gdgalgiers.com/static/phonelogo-db9c725b1463afd46d9b886076124bb2.png'), ...(embeds.slice(1))],
-                        });
+                        if (embeds.length === 1) {
+                            // send the embeds
+                            channelName.send({
+                                embeds: [embeds[0].setDescription(m.content).setAuthor('GDG Algiers', 'https://www.gdgalgiers.com/static/phonelogo-db9c725b1463afd46d9b886076124bb2.png').setFooter(interact.author.username, interact.author.displayAvatarURL({ format: 'png' }))],
+                            });
+                        } else { // send the embeds
+                            channelName.send({
+                                embeds: [embeds[0].setDescription(m.content).setAuthor('GDG Algiers', 'https://www.gdgalgiers.com/static/phonelogo-db9c725b1463afd46d9b886076124bb2.png'), ...(embeds.slice(1, -1)), embeds[embeds.length - 1].setFooter(interact.author.username, interact.author.displayAvatarURL({ format: 'png' }))],
+                            });
+                        }
                     } else {
                         await channelName.send(m.content);
                     }
-                    //confirm that the message has been announced
+                    // confirm that the message has been announced
                     await interaction.followUp(ephemeral(`Your message has been successfully announced at ${channelName}`));
                 }
             });
-            collector.on('end', async collected => {
+            collector.on('end', async(collected) => {
                 if (collected.size === 0) {
-                    await interaction.followUp(ephemeral("I didn\'t receive your announcement :/"));
+                    await interaction.followUp(ephemeral('I didn\'t receive your announcement :/'));
                 }
                 return;
             });
