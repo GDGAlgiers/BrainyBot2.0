5f362b2b1cf57bce96648bb18cf4af5d4f7f8b1c
Elhaouari Mohammmed <9967336+mohammed-elhaouari@users.noreply.github.com>
2022-03-21 17:24:53 +0100

    Add a new command to fetch a picture from pixels

---
+++ src/commands/fun/photo.js
@@ -0,0 +1,51 @@
+const fetch = (...args) =>
+  import("node-fetch").then(({ default: fetch }) => fetch(...args));
+const { MessageEmbed } = require("discord.js");
+
+module.exports = {
+  name: "photo",
+  description: "Get a photo from pixels.com",
+  options: [
+    {
+      name: "query",
+      description: "The search query. Ocean, Tigers, Pears, etc.",
+      type: "STRING",
+      required: false,
+    },
+  ],
+  required: true,
+  execute: async (client, interaction, args) => {
+    // getting the image URL:
+    const query = interaction.options.getString("query") ?? "nature";
+    const url = `https://api.pexels.com/v1/search?query=${query}&per_page=20`;
+
+    try {
+      const res = await fetch(url, {
+        headers: {
+          Authorization: process.env.PIXELS_API_KEY, // replace with env
+        },
+      });
+      const { photos = [] } = await res.json();
+      const photo = randomPhoto(photos);
+      const ImageEmbed = new MessageEmbed()
+        .setColor(photo?.avg_color)
+        .setTitle(photo?.alt)
+        .setImage(photo?.src?.large)
+        .setTimestamp();
+      await interaction.reply({ embeds: [ImageEmbed] });
+    } catch (error) {
+      const adviceEmbed = new MessageEmbed()
+        .setTitle("No pictures for you (┛ಠ_ಠ)┛彡┻━┻")
+        .setColor("#ff2c40")
+        .setTimestamp();
+      await interaction.reply({ embeds: [adviceEmbed] });
+    }
+  },
+};
+
+function randomPhoto(photos) {
+  if (!photos.length) {
+    throw new Error("No pictures found");
+  }
+  return photos[Math.floor(Math.random() * photos.length)];
+}
